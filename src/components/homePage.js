01132731630d2836180aa41599247ffd58008c9e
nikhil bhatia <nikhilbhatia2004@gmail.com>
2019-05-08 14:27:38 +0300

    jumbo 2

--- src/components/homePage.js
+++ src/components/homePage.js
@@ -3,11 +3,13 @@ import React from 'react';
 import NavigationBar from './navbar/navbar'
 import { Container, Row, Col } from 'react-bootstrap'
 import  Jumbo  from './jumbo/jumbo'
+const heading = "Keep me updated about devices, news, tips and offers from Google Store."
 const HomePage = (props) => {
     return (
       <div>
         <NavigationBar />
-        <Jumbo />
+        <Jumbo style='banner' />
+        <Jumbo style='jumbo2' heading={heading}/>
       </div>
     )
 
--- src/components/jumbo/jumbo.js
+++ src/components/jumbo/jumbo.js
@@ -2,10 +2,13 @@ import React from 'react'
 import Jumbotron from 'react-bootstrap/Jumbotron'
 import "./jumbo.css"
 const Jumbo = (props) => {
+  const { style, heading } = props
   return (
     <div>
-      <Jumbotron className="banner">
-  
+      <Jumbotron className={style}>
+        <h2>
+          { heading }
+        </h2>
       </Jumbotron>
   </div>
   )
